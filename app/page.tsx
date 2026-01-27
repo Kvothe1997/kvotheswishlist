8935db67a38b06e437b7a44b8c538862c8d604a3
Kvothe <barreto0608@gmail.com>
2026-01-26 23:23:00 -0500

    new items

--- app/page.tsx
+++ app/page.tsx
@@ -20,6 +20,18 @@ export default function Home() {
             price={"69.99"}
           />
           <Card
+            shopLink={"https://www.apple.com/shop/buy-iphone/iphone-17"}
+            imageLink={"https://store.storeimages.cdn-apple.com/1/as-images.apple.com/is/iphone-17-finish-select-202509-mistblue_GEO_US?wid=5120&hei=2880&fmt=webp&qlt=90&.v=WGdCRlQ0YVlqbTdXTEkxRnVQb0oxcFYyWWhPSUg0YytZdmJ2dmY4d09xckN0VFdyaFlNakY5MGMxMWhINEhMWmxjZnhHRHJyenVmME5KTm9Sd1ZaU3NqbWRhTGpRM2xxVWJRWUhSaDlCQ3JHYmE3Q0tucGdwdjhDQ1JZbjRxQXRka0xmckVNVTBkS20yTzkwa0dhU09n&traceId=1"}
+            title={"Iphone 17"}
+            price={"829"}
+          />
+          <Card
+            shopLink={"https://www.amazon.com/CMF-NOTHING-Watch-Pro-Bluetooth/dp/B0F66VB1SV/ref=sr_1_1?crid=14UDGXBTMHZXU&dib=eyJ2IjoiMSJ9.Nmit4afTLnpX6oYrtFJ6sGM9BIowv-KfgrodLfpumMME0fuisFrHaO4ZrRr5IeGRZ8n2ic-2pm1OQAbOrVR2XFGPSU3SnrrpeJB7Q6jq1HbVsucXBuyl5Ed6_utPbYML7nyI11glLeU2JnV-79R0mF4KEYkfQM2Ju6LP3UG6uAUuNLFoGkh5txlD3MxFUfXpzIYbWTC6I6euJLmKf6Urg9Zj1C9Mt-Z1Qbf8mnMsFhU.7h5XfFwzovGCtfr7x25A2QOpsTyEsJGbWQS9hQzxeAQ&dib_tag=se&keywords=cmf%2Bwatch&qid=1769487668&sprefix=cmf%2Bwatch%2Caps%2C305&sr=8-1&th=1"}
+            imageLink={"https://m.media-amazon.com/images/I/51nKhB-VZUL._AC_SL1010_.jpg"}
+            title={"CMF by Nothing Watch 3 Pro Smart Watch"}
+            price={"63.20"}
+          />
+          <Card
             shopLink={"https://www.amazon.com/Magic-Gathering-Commanders-Borderless-Accessories/dp/B0FHJJS17L/ref=sr_1_1?crid=25B8116MSZA7J&dib=eyJ2IjoiMSJ9.ybOiyI7m8EQxmkTBPomE0qLwe9IDbA2LQOEdDXk8mgif4CzPJnp3UgeCexRUENh825QSSBtHHpfjWd_9ZQWs3Y1IFIKAWDxjX_KX6u_KADs1a6gHKdbTlgpiqYhkIlHvL13dJ5jimiljx9Op1zvdJUGxE7nw2cG0F5Q6JJ8V1hMoZPVuxfiAU1ky9VSqFd9taG353N2pJVFOhayRSzU7DE9UuVss2Vi8qmfmxxKKulElDLNzm9SCtSMkvKoXun7XPkFoqg-qTuFfErrkDj-ZGNZwddlt6cMVPmrCXBQBO6o.p7xftl_AD4GJdAVjc-WW9BftnACWi66eXEY9QyHPF-Q&dib_tag=se&keywords=commander+deck+avatar&qid=1769225266&sprefix=commander+deck+avat%2Caps%2C274&sr=8-1"}
             imageLink={"https://m.media-amazon.com/images/I/81hXZ1Ib6gL._AC_SL1500_.jpg"}
             title={"Magic: The Gathering | Avatar: The Last Airbender Commander’s Bundle"}
