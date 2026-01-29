2aa59f76b5e624f99fa32c61efe80ffa43608993
Kvothe <barreto0608@gmail.com>
2026-01-28 22:09:41 -0500

    Added items

--- app/page.tsx
+++ app/page.tsx
@@ -14,12 +14,6 @@ export default function Home() {
         </div>
         <div className="flex flex-col justify-center items-center gap-6 text-center sm:items-start sm:text-center">
           <Card
-            shopLink={"https://www.amazon.com/Magic-Gathering-Airbender-Accessories-Collectible/dp/B0FHJGC6N7/ref=sr_1_28?crid=34FPBALT9UPRX&dib=eyJ2IjoiMSJ9.ybOiyI7m8EQxmkTBPomE0rpAJd_luJAD1UIkIDCRgblfErPRmFw8IVRFL0E7c-Wvv6h5rl9PbjfJN53dRDrAi9leYXnWv_B7lWIDPFqIr9VF8uj-P8hzBygoKuV73wDvytoodC0GDT_qFRZ3klO1wE3uh3U_d8yAFNV9fZnU7XxFeTzPgRD2Lh-YGFmmeESFa2fXA-nEe57JTQQvwzAW07NLtlQfV4-sZB4r6NkWlt9oQUfof1My_JinFsxl1nWydOuyVURn0vOJWr4s41ht6rZaikAmnSk_axow1p08AOQ.hGHJEDmE4mTHl0drQfr1eghfrLN22H7SX-PR5Hmc2PQ&dib_tag=se&keywords=bundle+avatar+mtg&qid=1769225621&sprefix=bundle+avatar+mt%2Caps%2C212&sr=8-28"}
-            imageLink={"https://m.media-amazon.com/images/I/71UwQseCCfL._AC_SL1500_.jpg"}
-            title={"Magic: The Gathering | Avatar: The Last Airbender Bundle"}
-            price={"69.99"}
-          />
-          <Card
             shopLink={"https://www.apple.com/shop/buy-iphone/iphone-17"}
             imageLink={"https://store.storeimages.cdn-apple.com/1/as-images.apple.com/is/iphone-17-finish-select-202509-mistblue_GEO_US?wid=5120&hei=2880&fmt=webp&qlt=90&.v=WGdCRlQ0YVlqbTdXTEkxRnVQb0oxcFYyWWhPSUg0YytZdmJ2dmY4d09xckN0VFdyaFlNakY5MGMxMWhINEhMWmxjZnhHRHJyenVmME5KTm9Sd1ZaU3NqbWRhTGpRM2xxVWJRWUhSaDlCQ3JHYmE3Q0tucGdwdjhDQ1JZbjRxQXRka0xmckVNVTBkS20yTzkwa0dhU09n&traceId=1"}
             title={"Iphone 17"}
@@ -30,6 +24,18 @@ export default function Home() {
             imageLink={"https://m.media-amazon.com/images/I/51nKhB-VZUL._AC_SL1010_.jpg"}
             title={"CMF by Nothing Watch 3 Pro Smart Watch"}
             price={"63.20"}
+          />
+          <Card
+            shopLink={"https://www.amazon.com/Magic-Gathering-Commander-Fantastic-Accessories/dp/B0G87CH7TH/ref=ast_sto_dp_puis"}
+            imageLink={"https://m.media-amazon.com/images/I/71TLDPyPWHL._AC_SL1500_.jpg"}
+            title={"Magic: The Gathering | Marvel Super Heroes Commander Deck - The Fantastic Four"}
+            price={"74.99"}
+          />
+          <Card
+            shopLink={"https://www.amazon.com/Magic-Gathering-Airbender-Accessories-Collectible/dp/B0FHJGC6N7/ref=sr_1_28?crid=34FPBALT9UPRX&dib=eyJ2IjoiMSJ9.ybOiyI7m8EQxmkTBPomE0rpAJd_luJAD1UIkIDCRgblfErPRmFw8IVRFL0E7c-Wvv6h5rl9PbjfJN53dRDrAi9leYXnWv_B7lWIDPFqIr9VF8uj-P8hzBygoKuV73wDvytoodC0GDT_qFRZ3klO1wE3uh3U_d8yAFNV9fZnU7XxFeTzPgRD2Lh-YGFmmeESFa2fXA-nEe57JTQQvwzAW07NLtlQfV4-sZB4r6NkWlt9oQUfof1My_JinFsxl1nWydOuyVURn0vOJWr4s41ht6rZaikAmnSk_axow1p08AOQ.hGHJEDmE4mTHl0drQfr1eghfrLN22H7SX-PR5Hmc2PQ&dib_tag=se&keywords=bundle+avatar+mtg&qid=1769225621&sprefix=bundle+avatar+mt%2Caps%2C212&sr=8-28"}
+            imageLink={"https://m.media-amazon.com/images/I/71UwQseCCfL._AC_SL1500_.jpg"}
+            title={"Magic: The Gathering | Avatar: The Last Airbender Bundle"}
+            price={"69.99"}
           />
           <Card
             shopLink={"https://www.amazon.com/Magic-Gathering-Commanders-Borderless-Accessories/dp/B0FHJJS17L/ref=sr_1_1?crid=25B8116MSZA7J&dib=eyJ2IjoiMSJ9.ybOiyI7m8EQxmkTBPomE0qLwe9IDbA2LQOEdDXk8mgif4CzPJnp3UgeCexRUENh825QSSBtHHpfjWd_9ZQWs3Y1IFIKAWDxjX_KX6u_KADs1a6gHKdbTlgpiqYhkIlHvL13dJ5jimiljx9Op1zvdJUGxE7nw2cG0F5Q6JJ8V1hMoZPVuxfiAU1ky9VSqFd9taG353N2pJVFOhayRSzU7DE9UuVss2Vi8qmfmxxKKulElDLNzm9SCtSMkvKoXun7XPkFoqg-qTuFfErrkDj-ZGNZwddlt6cMVPmrCXBQBO6o.p7xftl_AD4GJdAVjc-WW9BftnACWi66eXEY9QyHPF-Q&dib_tag=se&keywords=commander+deck+avatar&qid=1769225266&sprefix=commander+deck+avat%2Caps%2C274&sr=8-1"}
--- next.config.ts
+++ next.config.ts
@@ -6,7 +6,10 @@ const nextConfig: NextConfig = {
     new URL('https://m.media-amazon.com/images/I/81hXZ1Ib6gL._AC_SL1500_.jpg'),
     new URL('https://m.media-amazon.com/images/I/71UwQseCCfL._AC_SL1500_.jpg'),
     new URL('https://m.media-amazon.com/images/I/71Y+xb97+XL._AC_SL1500_.jpg'),
-    new URL('https://m.media-amazon.com/images/I/71Y997rEAKL._AC_SL1500_.jpg')],
+    new URL('https://m.media-amazon.com/images/I/71Y997rEAKL._AC_SL1500_.jpg'),
+    new URL('https://m.media-amazon.com/images/I/51nKhB-VZUL._AC_SL1010_.jpg'),
+    new URL('https://m.media-amazon.com/images/I/71TLDPyPWHL._AC_SL1500_.jpg'),
+    new URL('https://store.storeimages.cdn-apple.com/1/as-images.apple.com/is/iphone-17-finish-select-202509-mistblue_GEO_US?wid=5120&hei=2880&fmt=webp&qlt=90&.v=WGdCRlQ0YVlqbTdXTEkxRnVQb0oxcFYyWWhPSUg0YytZdmJ2dmY4d09xckN0VFdyaFlNakY5MGMxMWhINEhMWmxjZnhHRHJyenVmME5KTm9Sd1ZaU3NqbWRhTGpRM2xxVWJRWUhSaDlCQ3JHYmE3Q0tucGdwdjhDQ1JZbjRxQXRka0xmckVNVTBkS20yTzkwa0dhU09n&traceId=1')],
   },
 };
 
