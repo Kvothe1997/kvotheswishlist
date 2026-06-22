c45bc13ac198075de7fad6d377f7424154762db2
Kvothe <barreto0608@gmail.com>
2026-06-21 22:57:18 -0500

    Play 5

--- next.config.ts
+++ next.config.ts
@@ -17,7 +17,9 @@ const nextConfig: NextConfig = {
     new URL('https://http2.mlstatic.com/D_NQ_NP_2X_680425-MPE109561080444_042026-F.webp'),
     new URL('https://www.mercadolibre.com.pe/afeitadora-my-balls-40-pro/up/MPEU2458661675?pdp_filters=item_id%3AMPE659701510&from=gshop&matt_tool=25903432&matt_word=&matt_source=google&matt_campaign_id=23396607108&matt_ad_group_id=190389139123&matt_match_type=&matt_network=g&matt_device=c&matt_creative=790000007698&matt_keyword=&matt_ad_position=&matt_ad_type=pla&matt_merchant_id=5082135693&matt_product_id=MPEU2458661675&matt_product_partition_id=2454652782921&matt_target_id=aud-2457655439684:pla-2454652782921&cq_src=google_ads&cq_cmp=23396607108&cq_net=g&cq_plt=gp&cq_med=pla&gad_source=1&gad_campaignid=23396607108&gbraid=0AAAAADt7RJLuj-fw1g7rp6huImGN3wX0j&gclid=CjwKCAjw0dPRBhAPEiwAE5vTTqQ7fZE2nk--UkGh63OrvE2EhfAJylhAAXU3Ygw4IHthoLwVb8Y80RoCWnkQAvD_BwE'),
     new URL('https://diadejuegos.pe/euro-games/3605-catan-duelo.html'),
-    new URL('https://diadejuegos.pe/3368-large_default/catan-duelo.jpg')],
+    new URL('https://diadejuegos.pe/3368-large_default/catan-duelo.jpg'),
+    new URL('https://media.falabella.com/falabellaPE/153390813_01/w=1200,h=1200,fit=pad'),
+    new URL('https://www.falabella.com.pe/falabella-pe/product/153390812/consola-playstation-5-slim-digital-sony/153390813?kid=shopp44fc&gclsrc=aw.ds&gad_source=1&gad_campaignid=20114538213&gbraid=0AAAAADs9MO2aooNxTq_tGr64QgGunHWpL&gclid=CjwKCAjwl97RBhBWEiwAa9rbXeKv8YDbI2HaCAAEI1GxYnSNMxtVQjJo8eb2RPy4h4d7colXoK__phoCfuwQAvD_BwE')],
   },
 };
 
